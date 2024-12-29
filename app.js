f942064ddd774fec7cedf935fb1e53d3ebbbc994
alamin-php <alamin.php100@gmail.com>
2024-12-29 15:17:30 +0600

    index.html added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
+const cors = require("cors");
 
 const {
   GoogleGenerativeAI,
@@ -15,6 +16,15 @@ const PORT = 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+const corsOpts = {
+  origin: "*",
+
+  methods: ["GET", "POST"],
+
+  allowedHeaders: ["Content-Type", "api_token"],
+};
+
+app.use(cors(corsOpts));
 
 const verifyAccessKey = (req, res, next) => {
   const providedKey = req.headers["api_token"];
@@ -26,8 +36,6 @@ const verifyAccessKey = (req, res, next) => {
 
   next();
 };
-
-
 
 // Initialize Google Generative AI and File Manager
 const apiKey = process.env.GEMINI_API_KEY;
@@ -62,9 +70,9 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   try {
-    res.json({ success: "Welcome to Odo Metter API" });
+    res.sendFile(path.join(__dirname, "public", "index.html"));
   } catch (error) {
     res.status(500).send("An error occurred.");
   }
@@ -93,70 +101,72 @@ const upload = multer({
 });
 
 // POST endpoint for processing requests
-app.post("/api/upload-analyze",verifyAccessKey, upload.single("file"), async (req, res) => {
-  try {
-    const { file } = req;
-    const {
-      userMessage = "Analyze the input image and extract the total odometer reading. Return the result in the following JSON format:{ 'total_km': 'The odo number' }",
-    } = req.body;
-
-    if (!file) {
-      return res.status(400).json({ error: "File is required" });
-    }
-
-    const filePath = file.path; // Path of the uploaded file
-    const mimeType = file.mimetype;
-
-    // Upload the file to Gemini
-    const geminiFile = await uploadToGemini(filePath, mimeType);
-
-    // Start a chat session
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-        {
-          role: "user",
-          parts: [
-            {
-              fileData: {
-                mimeType: geminiFile.mimeType,
-                fileUri: geminiFile.uri,
-              },
-            },
-            { text: userMessage },
-          ],
-        },
-      ],
-    });
-
-    // Send the user's message and get the model's response
-    const result = await chatSession.sendMessage(userMessage);
-    let responseText = result.response.text();
-
-    // Remove any code block markers (triple backticks) from the response text
-    responseText = responseText.replace(/```json\n|\n```/g, "").trim();
-
-    let responseJson = {};
+app.post( "/api/upload-analyze", verifyAccessKey, upload.single("file"),
+  async (req, res) => {
     try {
-      const parsedResponse = JSON.parse(responseText); // Parse the model's response
-      responseJson = {
-        status: 200, // Add the desired status
-        ...parsedResponse, // Merge the parsed response (e.g., `{ "total_km": "2500000" }`)
-      };
-    } catch (e) {
-      console.error("Error parsing response:", e);
-      responseJson = { status: "error", message: "Invalid response format" };
-    }
+      const { file } = req;
+      const {
+        userMessage = "Analyze the input image and extract the total odometer reading. Return the result in the following JSON format:{ 'total_km': 'The odo number' }",
+      } = req.body;
 
-    // Send back the parsed result
-    res.json(responseJson);
-  } catch (error) {
-    console.error("Error:", error);
-    res
-      .status(500)
-      .json({ error: "An error occurred", details: error.message });
+      if (!file) {
+        return res.status(400).json({ error: "File is required" });
+      }
+
+      const filePath = file.path; // Path of the uploaded file
+      const mimeType = file.mimetype;
+
+      // Upload the file to Gemini
+      const geminiFile = await uploadToGemini(filePath, mimeType);
+
+      // Start a chat session
+      const chatSession = model.startChat({
+        generationConfig,
+        history: [
+          {
+            role: "user",
+            parts: [
+              {
+                fileData: {
+                  mimeType: geminiFile.mimeType,
+                  fileUri: geminiFile.uri,
+                },
+              },
+              { text: userMessage },
+            ],
+          },
+        ],
+      });
+
+      // Send the user's message and get the model's response
+      const result = await chatSession.sendMessage(userMessage);
+      let responseText = result.response.text();
+
+      // Remove any code block markers (triple backticks) from the response text
+      responseText = responseText.replace(/```json\n|\n```/g, "").trim();
+
+      let responseJson = {};
+      try {
+        const parsedResponse = JSON.parse(responseText); // Parse the model's response
+        responseJson = {
+          status: 200, // Add the desired status
+          ...parsedResponse, // Merge the parsed response (e.g., `{ "total_km": "2500000" }`)
+        };
+      } catch (e) {
+        console.error("Error parsing response:", e);
+        responseJson = { status: "error", message: "Invalid response format" };
+      }
+
+      // Send back the parsed result
+      res.json(responseJson);
+    } catch (error) {
+      console.error("Error:", error);
+      res
+        .status(500)
+        .json({ error: "An error occurred", details: error.message });
+    }
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
